3b6b22629befe4d20ac8521db7937d5093f90c8d
Augusto Moura <augusto.borgesm@gmail.com>
2017-03-04 21:36:00 -0300

    added nav style

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 export default function () {
+  const navStyle = {
+    backgroundColor: '#ddd',
+    fontSize: '1.1em',
+    padding: '.8em',
+  };
+
+  const linkStyle = {
+    color: '#3b8ad2',
+    display: 'inline-block',
+    padding: '0 .5em',
+    textDecoration: 'none',
+  };
+
+  const activeLinkStyle = {
+    color: '#000',
+    fontWeight: 'bold',
+  };
+
   return (
-    <div>
-      <h2>Nav component</h2>
-      <Link to="/">Get weather</Link>
-      <Link to="/about">About</Link>
-      <Link to="/examples">Examples</Link>
+    <div style={navStyle}>
+      <IndexLink to="/" style={linkStyle} activeStyle={activeLinkStyle}>Get weather</IndexLink>
+      <Link to="/about" style={linkStyle} activeStyle={activeLinkStyle}>About</Link>
+      <Link to="/examples" style={linkStyle} activeStyle={activeLinkStyle}>Examples</Link>
     </div>
   );
 }
