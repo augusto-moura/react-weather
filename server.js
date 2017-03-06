1891a1eebc4d9e06856cb88fbdd1c1a150ba23a0
Augusto Moura <augusto.borgesm@gmail.com>
2017-03-06 16:44:32 -0300

    Preparation for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
-const PORTA = 3000;
+const PORTA = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('public'));
-
-app.listen(PORTA, () => console.log(`Servidor subiu na porta: ${PORTA}`));
+app
+  .use((req, res, next) => {
+    if (req.headers['x-forwaded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect(`http://${req.hostname + req.url}`);
+    }
+  })
+  .use(express.static('public'))
+  .listen(PORTA, () => console.log(`Servidor subiu na porta: ${PORTA}`));
