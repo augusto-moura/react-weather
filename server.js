982f5f3338f9b717bfb2d1ba2225089041d8b8b2
Augusto Moura <augusto.borgesm@gmail.com>
2017-03-06 16:49:22 -0300

    Fixed error on `server.js`

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const PORTA = process.env.PORT || 3000;
 
 const app = express();
 
-app
-  .use((req, res, next) => {
-    if (req.headers['x-forwaded-proto'] === 'http') {
-      next();
-    } else {
-      res.redirect(`http://${req.hostname + req.url}`);
-    }
-  })
-  .use(express.static('public'))
-  .listen(PORTA, () => console.log(`Servidor subiu na porta: ${PORTA}`));
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect(`http://${req.hostname + req.url}`);
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORTA, () => console.log(`Servidor subiu na porta: ${PORTA}`));
