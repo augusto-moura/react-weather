bc77d9514a6aeaf861b9d60d1a9564017c510e3b
augusto-moura <augusto.borgesm@gmail.com>
2017-03-08 11:33:43 -0300

    Added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
+import 'style!css!foundation-sites/dist/foundation.min.css';
+
 import Main from './components/Main';
 import ShowWeather from './components/ShowWeather';
 import ShowAbout from './components/ShowAbout';
 import ShowExamples from './components/ShowExamples';
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
